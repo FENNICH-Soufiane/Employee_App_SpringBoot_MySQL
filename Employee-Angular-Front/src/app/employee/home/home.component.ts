6191ee8384df056dcb8fbbdcec762764ef656524
FENNICH-Soufiane <fennich0011soufiane@gmail.com>
2024-10-18 14:41:09 +0100

    Add Table, Angular Table

--- Employee-Angular-Front/src/app/employee/home/home.component.ts
+++ Employee-Angular-Front/src/app/employee/home/home.component.ts
@@ -2,14 +2,21 @@ import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import {MatFormFieldModule} from '@angular/material/form-field'; 
 import {MatInputModule} from '@angular/material/input';
+import {MatTableDataSource, MatTableModule} from '@angular/material/table';
+import { Employee } from '../employee';
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MatFormFieldModule, MatInputModule, MatButtonModule],
+  imports: [MatFormFieldModule, MatInputModule, MatButtonModule, MatTableModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
 
+export class HomeComponent {
+  displayedColumns: string[] = ['id', 'name', 'email', 'salary'];
+  dataSource = new MatTableDataSource<Employee>();
+
+  employees:Employee[] = [];
 }
