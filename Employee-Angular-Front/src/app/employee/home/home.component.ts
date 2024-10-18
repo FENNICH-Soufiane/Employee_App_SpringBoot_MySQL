141cf0d4561b590d6f9e6b427b155a491a7d4c5b
FENNICH-Soufiane <fennich0011soufiane@gmail.com>
2024-10-18 15:44:53 +0100

    Angular Filter

--- Employee-Angular-Front/src/app/employee/home/home.component.ts
+++ Employee-Angular-Front/src/app/employee/home/home.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import {MatFormFieldModule} from '@angular/material/form-field'; 
-import {MatInputModule} from '@angular/material/input';
-import {MatTableDataSource, MatTableModule} from '@angular/material/table';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { Employee } from '../employee';
 import { EmployeeService } from '../employee.service';
 
@@ -19,14 +19,26 @@ export class HomeComponent {
   displayedColumns: string[] = ['id', 'name', 'email', 'salary'];
   dataSource = new MatTableDataSource<Employee>();
 
-  employees:Employee[] = [];
+  employees: Employee[] = [];
+  filteredEmployees: Employee[] = [];
 
-  constructor(private employeeService:EmployeeService) {}
+  constructor(private employeeService: EmployeeService) { }
 
-  ngOnInit():void {
+  ngOnInit(): void {
     this.employeeService.fetchAllEmployees().subscribe((data) => {
       this.employees = data;
       this.dataSource = new MatTableDataSource<Employee>(data);
     })
   }
+
+  searchEmployee(input: any) {
+    this.filteredEmployees = this.employees.filter(item =>
+      item.name.toLowerCase().includes(input.toLowerCase()) ||
+      item.email.toLowerCase().includes(input.toLowerCase()) ||
+      item.salary.toString().includes(input)
+      
+    )
+    this.dataSource = new MatTableDataSource<Employee>(this.filteredEmployees);
+  }
+
 }
